a69afccc86c4807dd15629e7932e6137e76bcdaf
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-11-16 11:50:23 -0500

    add exploratory XML parsing code for LearnersDictionary

--- mwapi.py
+++ mwapi.py
@@ -1,4 +1,9 @@
-from abc import ABCMeta, abstractmethod, abstractproperty
+import re
+import xml.etree.cElementTree as ElementTree
+
+from abc import ABCMeta, abstractmethod
+from urllib import quote, quote_plus
+from urllib2 import urlopen
 
 """
 undocumented: subj, formula, table
@@ -60,17 +65,56 @@ class MWApiWrapper:
                 if p:
                     parts.append(p)
         return ''.join(parts)
+
+class LearnersDictionary(MWApiWrapper):
+    def lookup(self, word):
+        response = urlopen(self.request_url(word))
+        data = response.read()
+        root = ElementTree.fromstring(data)
+        entries = root.findall("entry")
+        if not entries:
+            suggestions = root.findall("suggestion")
+            if suggestions:
+                suggestions = [s.text for s in suggestions]
+            raise WordNotFoundException(word, suggestions)
+        for num, entry in enumerate(entries):
+            args = {}
+            args['headword'] = entry.find("hw").text
+            sound = entry.find("sound")
+            if sound:
+                args['sound_fragments'] = [s.text for s in sound]
+            args['functional_label'] = entry.find('fl').text
+            args['senses'] = []
+            for definition in entry.findall('.//def/dt'):
+                dstring = self.stringify_tree(definition,
+                              lambda x: x.tag not in ['vi', 'wsgram'])
+                usage = [self.vi_to_text(u) for u in definition.findall('vi')]
+                args['senses'].append((dstring, usage))
+            yield LearnersDictionaryEntry(word, args)
+
+    def vi_to_text(self, root):
+        example = self.stringify_tree(root)
+        return re.sub(r'[=.*?]', '', example)
+
+    def request_url(self, word):
+        if self.key is None:
+            raise Exception("API key not set")
+        qstring = "{0}?key={1}".format(quote(word), quote_plus(self.key))
+        return ("http://www.dictionaryapi.com/api/v1/references/learners"
+                "/xml/{0}").format(qstring)
+
 class LearnersDictionaryEntry(object):
-    def __init__(self, attrs):
+    def __init__(self, word, attrs):
         # word,  pronounce, sound_url, art_url, inflection, pos
 
+        self.word = word
         self.headword = attrs.get("headword")
         self.alternate_headwords = attrs.get("alternate_headwords")
         self.pronunciations = attrs.get("pronunciations")
         self.functional_label = attrs.get("functional_label")
         # aka pos?
         self.inflections = attrs.get("inflections") # (form, [pr], note,)
-        self.definitions = attrs.get("definitions")
+        self.senses = attrs.get("senses")
         # list of (["def text"], ["examples"], ["notes"])
-        sound_fragment = attrs.get("sound_fragment")
+        sound_fragments = attrs.get("sound_fragments")
         art_fragment = attrs.get("art_fragment")
