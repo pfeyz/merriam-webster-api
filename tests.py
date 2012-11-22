1f9753a047e4be1d786690e8f3701a3c367ebec0
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-11-22 12:41:43 -0500

    mock urlopen in tests, use xml reponse fixtures

--- tests.py
+++ tests.py
@@ -4,13 +4,25 @@ from os import path
 
 from mwapi import LearnersDictionary, WordNotFoundException
 
-class LearnerTests(unittest.TestCase):
+class MerriamWebsterTestCase(unittest.TestCase):
+    def _mock_url_opener(self):
+        def opener(url):
+            fn = re.sub(r"^%s" % self.base_url, "", url)
+            fn = re.sub(r"\?.*$", "", fn)
+            fn = path.join(self.fixture_dir, "{0}.xml".format(fn))
+            return open(fn, 'r')
+        return opener
+
+class LearnerTests(MerriamWebsterTestCase):
     @classmethod
     def setUpClass(cls):
         cls.api_key = ""
+        cls.fixture_dir = "fixtures/learners"
+        cls.base_url = \
+            "http://www.dictionaryapi.com/api/v1/references/learners/xml/"
 
     def test_word_not_found(self):
-        d = LearnersDictionary(self.api_key)
+        d = LearnersDictionary(self.api_key, self._mock_url_opener())
         with self.assertRaises(WordNotFoundException):
             try:
                 list(d.lookup("murda"))
@@ -19,7 +31,7 @@ class LearnerTests(unittest.TestCase):
                 raise e
 
     def test_handle_malformed_xml(self):
-        d = LearnersDictionary(self.api_key)
+        d = LearnersDictionary(self.api_key, self._mock_url_opener())
         with self.assertRaisesRegexp(WordNotFoundException,
                                      r'.* not found\.( Try:.*)?$'):
             list(d.lookup("3rd"))
