f4ff9466443fa6abb163512347cd1c9654f2fee3
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-11-23 11:49:11 -0500

    move api lookup object instantiation to setUp

--- tests.py
+++ tests.py
@@ -23,20 +23,21 @@ class LearnerTests(MerriamWebsterTestCase):
         cls.base_url = \
             "http://www.dictionaryapi.com/api/v1/references/learners/xml/"
 
+    def setUp(self):
+        self.dictionary = LearnersDictionary(self.api_key,
+                                             self._mock_url_opener())
     def test_word_not_found(self):
-        d = LearnersDictionary(self.api_key, self._mock_url_opener())
         with self.assertRaises(WordNotFoundException):
             try:
-                list(d.lookup("murda"))
+                list(self.dictionary.lookup("murda"))
             except WordNotFoundException as e:
                 self.assertTrue(len(e.suggestions) > 0)
                 raise e
 
-    def test_handle_malformed_xml(self):
-        d = LearnersDictionary(self.api_key, self._mock_url_opener())
+    def test_handle_malformed_xmlq(self):
         with self.assertRaisesRegexp(WordNotFoundException,
                                      r'.* not found\.( Try:.*)?$'):
-            list(d.lookup("3rd"))
+            list(self.dictionary.lookup("3rd"))
 
 if __name__ == '__main__':
     unittest.main()
