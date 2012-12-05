e6054b84efde5f91912b15cba028fe1e908fb3f2
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-12-05 07:37:24 -0500

    improve ipa example

--- examples/ipa.py
+++ examples/ipa.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 """
 Simple command-line tool that fetches the IPA (International Phonetic Alphabet)
 transcriptions for a given word from Merriam Webster.
@@ -9,24 +11,24 @@ transcriptions for a given word from Merriam Webster.
   $ python2 ipa.py xml
   ˌɛksˌɛmˈɛl
 
-For simplicities sake, WordNotFound exceptions thrown by
-LearnersDictionary.lookup are not handled.
-
 """
 
 import os
 import sys
 
-from merriam_webster.api import LearnersDictionary
+from merriam_webster.api import LearnersDictionary, WordNotFoundException
 
 if __name__ == "__main__":
     query = " ".join(sys.argv[1:])
     key = os.getenv("MERRIAM_WEBSTER_LEARNERS_KEY")  # gets your api key
     learners = LearnersDictionary(key)
-    ipas = [ipa
-            for entry in learners.lookup(query)
-            for ipa in entry.pronunciations]
-    for ipa in set(ipas):
+    try:
+        ipas = [ipa
+                for entry in learners.lookup(query)
+                for ipa in entry.pronunciations]
+    except WordNotFoundException:
+        ipas = []
+    for ipa in set(ipas):  # word in MW but no IPA for it
         print ipa
     if not ipas:
         print "No transcriptions found for '{0}'".format(query)
