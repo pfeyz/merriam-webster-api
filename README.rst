9e1bb7b4214f630054af3abac881dddb78c55a70
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-12-19 12:22:18 -0500

    update documentation

--- README.rst
+++ README.rst
@@ -6,21 +6,21 @@ A wrapper around the Merriam-Webster APIs as Python module.
 
 You'll need to get API keys from `Merriam-Webster's Developer Center`_.
 
-Currently only the Learner's Dictionary is implemented. Support for the
-Collegiate Dictionary and Thesaurus are planned, but note that Merriam-Webster's
-`terms of service`_ forbid use of more than two of their reference works via the
-APIs.
+The Learner's Dictionary is supported and there is a preliminary implementation
+of the Collegiate Dictionary. Support for the Thesaurus is planned, but note
+that Merriam-Webster's `terms of service`_ forbid use of more than two of their
+reference works via the APIs.
 
 I was initially interested in the API for the IPA_ transcriptions that
-Merriam-Webster provides. I targeted the Learner's Dictionary which uses IPA
+Merriam-Webster provides. I started with the Learner's Dictionary which uses IPA
 unlike MW's Collegiate Dictionary which uses a `non-ipa pronunciation
 notation`_.
 
-See examples/ipa.py for an example script using the module to fetch IPA
-transcriptions.
+See examples/ for usage examples.
 
-The example script and tests expect to find your Learner's Dictionary API key in
-the ``MERRIAM_WEBSTER_LEARNERS_KEY`` environmental variable.
+The example scripts and tests expect to find your API keys in the
+``MERRIAM_WEBSTER_LEARNERS_KEY`` and/or ``MERRIAM_WEBSTER_COLLEGIATE_KEY``
+environmental variables.
 
 
 .. _`Merriam-Webster's Developer Center`: http://www.dictionaryapi.com/
--- examples/define.py
+++ examples/define.py
@@ -5,18 +5,39 @@ Simple command-line tool that fetches the dictionary definitions of a given word
 from Merriam Webster.
 
   $ python define.py bike
-  bike [noun]: bicycle
-  bike [noun]: motorcycle
-  bike [verb]: to ride a bicycle
+  LEARNERS: bike [noun]: bicycle
+  LEARNERS: bike [noun]: motorcycle
+  LEARNERS: bike [verb]: to ride a bicycle
+  COLLEGIATE: bike [noun]: a nest of wild bees, wasps, or hornets
+  COLLEGIATE: bike [noun]: a crowd or swarm of people
+  COLLEGIATE: bike [noun]: bicycle
+  COLLEGIATE: bike [noun]: motorcycle
+  COLLEGIATE: bike [noun]: motorbike
+  COLLEGIATE: bike [noun]: stationary bicycle
+  COLLEGIATE: bike [verb]: to ride a bike
 
   $ python define.py definition
-  definition [noun]: an explanation of the meaning of a word, phrase, etc.; a statement that defines a word, phrase, etc.
-  definition [noun]: a statement that describes what something is
-  definition [noun]: a clear or perfect example of a person or thing
-  definition [noun]: the quality that makes it possible to see the shape, outline, and details of something clearly
+  LEARNERS: definition [noun]: an explanation of the meaning of a word, phrase, etc.; a statement that defines a word, phrase, etc.
+  LEARNERS: definition [noun]: a statement that describes what something is
+  LEARNERS: definition [noun]: a clear or perfect example of a person or thing
+  LEARNERS: definition [noun]: the quality that makes it possible to see the shape, outline, and details of something clearly
+  COLLEGIATE: definition [noun]: an act of determining
+  COLLEGIATE: definition [noun]: the formal proclamation of a Roman Catholic dogma
+  COLLEGIATE: definition [noun]: a statement expressing the essential nature of something
+  COLLEGIATE: definition [noun]: a statement of the meaning of a word or word group or a sign or symbol
+  COLLEGIATE: definition [noun]: a product of defining
+  COLLEGIATE: definition [noun]: the action or process of stating the meaning of a word or word group
+  COLLEGIATE: definition [noun]: the action or the power of describing, explaining, or making definite and clear
+  COLLEGIATE: definition [noun]: clarity of visual presentation; distinctness of outline or detail
+  COLLEGIATE: definition [noun]: clarity especially of musical sound in reproduction
+  COLLEGIATE: definition [noun]: sharp demarcation of outlines or limits
+  COLLEGIATE: definition [adjective]: being or relating to an often digital television system that has twice as many scan lines per frame as a conventional system, a proportionally sharper image, and a wide-screen format
+  COLLEGIATE: definition [noun]: the evaluation by oneself of one's worth as an individual in distinction from one's interpersonal or social roles
+
 
   $ python define.py refactor
-  No definitions found for 'refactor'
+  LEARNERS: No definitions found for 'refactor'
+  COLLEGIATE: No definitions found for 'refactor'
 
 """
 
