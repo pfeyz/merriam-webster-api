a8a7808d2d99f96d48f401cc3b5341b9aaee0d40
Dominik Madarasz <madaraszd@mail.ru>
2017-07-26 13:16:42 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(  name='Merriam-Webster API',
+        version='1.0',
+        description='Merriam-Webster Dictionary API',
+        author='pfeyz',
+        url='https://github.com/pfeyz/merriam-webster-api',
+        packages=['merriam_webster'])
